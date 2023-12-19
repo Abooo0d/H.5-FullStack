dbcb2b884a3a372518feb58599c1c377a7e2c875
Aboo00d <abdsadalden2001@gmail.com>
2023-12-19 12:24:47 +0200

    Finishing Up For Deployment

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/dashboard",
   plugins: [react()],
-  server: {
-    host: "192.168.105.159",
-    port: 3000,
-  },
+  base: "/H.5-FullStack/",
 });
