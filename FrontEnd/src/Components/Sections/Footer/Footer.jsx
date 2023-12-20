ee0ba5acff5fc639c9d4ee0ab4ff00a2b10bed70
Aboo00d <abdsadalden2001@gmail.com>
2023-12-20 22:20:16 +0200

    Fixing The Footer One The Mobile

--- FrontEnd/src/Components/Sections/Footer/Footer.jsx
+++ FrontEnd/src/Components/Sections/Footer/Footer.jsx
@@ -17,11 +17,11 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-overlay">
-        <div className="logo">
+        <div className="footer-section logo">
           <img src={logo} alt="logo" />
           <h3>H.5 Shop</h3>
         </div>
-        <div className="links">
+        <div className="footer-section links">
           <h3 className="subtitle">Links</h3>
           <a href="#home">Home</a>
           <a href="#categories">Categories</a>
@@ -29,14 +29,14 @@ const Footer = () => {
           <a href="#quote">Quote</a>
           <a href="#contact-us">Contact Us</a>
         </div>
-        <div className="footer-categories">
+        <div className="footer-section footer-categories">
           <h3 className="subtitle">Categories</h3>
           <MiniCategory cat="Pages" Icon={HiMiniShoppingBag} />
           <MiniCategory cat="Clothes" Icon={PiPantsFill} />
           <MiniCategory cat="Shoes" Icon={GiChelseaBoot} />
           <MiniCategory cat="Accessories" Icon={GiNecklaceDisplay} />
         </div>
-        <div className="footer-accounts">
+        <div className="footer-section footer-accounts">
           {/* <h3 className="subtitle">Contact Us</h3> */}
           <div className="accounts-con">
             <MiniContact Icon={FaTiktok} />
--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/H.5-FullStack/",
-  server: { host: "192.168.43.5", port: "3000" },
+  base: "/H.5-FullStack/",
+  // server: { port: "3000" },
 });
